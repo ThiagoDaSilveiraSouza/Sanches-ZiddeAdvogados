f4e0d1f524153fbb514b157f7a359b5d1aee3e0e
Paula <plnhschltz@gmail.com>
2022-08-25 23:19:32 -0300

    inicio Banner

---
+++ src/pages/Home/components/MainBanner/style.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const MainBannerContent = styled.section`
+display: flex;
+flex: 1 1;
+background-image: url(bannerProvisorio.jpg);
+background-repeat: no-repeat;
+background-size: cover;
+background-position: center;
+height: 770px;
+align-items: center;
+`
+export const TextContent = styled.div`
+margin-left: 20%;
+/* background-color: blue; */
+/* max-width: 559px; */
+
+
+div{
+    p{
+        margin: 0;
+        padding: 0;
+        color: white;
+    }
+    h1{
+        margin: 0;
+        padding-top:10px;
+        color: #A38334;
+        font-size: calc(0.75em + 1vw);
+        padding-bottom: 30px;
+    }
+
+
+}
+`
