a1153b3b06cd57604d1c9a9520054b9d138933c8
ThiagoSouza <thiagodasilveirasouza@gmail.com>
2022-08-25 23:12:02 -0300

    update fonts and colors

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,18 +2,25 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root{
+    
+    /* font */
+    --primary-font: 'Mukta', sans-serif;
+
+    /* color */
     --dark-color: #191919;
-    --gold-color: rgb(163 131 52);
-    --gray-color: #e8e8e8;
+    --gold-color: #A38334;
+    --gray-color: #E8E8E8;
     --gray-color-2: rgb(232 232 232);
+
+
   }
   body {
     margin: 0;
     padding: 0;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-family:var(--primary-font);
   }
   .centralizer{
-    width: 800px;
+    width: 1140px;
     max-width: 90vw;
     margin: 0 auto;
   }
