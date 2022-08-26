f9c733bb837f95736475bf0d4814c749b9fe4d10
Paula <plnhschltz@gmail.com>
2022-08-26 16:49:18 -0300

    fonts

--- src/pages/Home/components/Credentials/Credentials.jsx
+++ src/pages/Home/components/Credentials/Credentials.jsx
@@ -8,6 +8,7 @@ export const Credentials = () => {
       </ImgContent>
       <TextCredentialsContent>
         <h2>Dra. Rachel Sanches Zidde</h2>
+        <hr/>
         <h5>SÓCIA FUNDADORA DO ESCRITÓRIO SANCHES & ZIDDE ADVOGADOS</h5>
         <p>
           Advogada formada pela UNIFESO- Centro Universitário Serra dos Órgãos,
