68cdd07b3bdb33659daa4b16257945a4fc436b1c
Paula <plnhschltz@gmail.com>
2022-08-27 02:08:18 -0300

    add centralizer on OurMission component

--- src/pages/Home/components/OurMission/OurMission.tsx
+++ src/pages/Home/components/OurMission/OurMission.tsx
@@ -4,16 +4,18 @@ import { BetterCardList } from "./data";
 
 export const OurMission = () => {
   return (
-    <OurMissionContent className="centralizer">
-      <OurMissionDiv>
-        {BetterCardList.map((card, index) => (
-          <CardMission key={index + "-" + card.title}>
-            <img src={card.imgPath} alt={"imagem de " + card.title} />
-            <h5>{card.title}</h5>
-            <p>{card.description}</p>
-          </CardMission>
-        ))}
-      </OurMissionDiv>
+    <OurMissionContent>
+      <div className="centralizer">
+        <OurMissionDiv>
+          {BetterCardList.map((card, index) => (
+            <CardMission key={index + "-" + card.title}>
+              <img src={card.imgPath} alt={"imagem de " + card.title} />
+              <h5>{card.title}</h5>
+              <p>{card.description}</p>
+            </CardMission>
+          ))}
+        </OurMissionDiv>
+      </div>
     </OurMissionContent>
   );
 };
