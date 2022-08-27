b9610ed0d5b82bed2228c1705648d9acd03f85aa
Paula <plnhschltz@gmail.com>
2022-08-27 01:25:32 -0300

    refactory with Thiago

--- src/pages/Home/components/WhoWheAre/style.ts
+++ src/pages/Home/components/WhoWheAre/style.ts
@@ -3,27 +3,40 @@ import styled from "styled-components";
 export const WhoWheAreContent = styled.section`
   display: flex;
   flex-wrap: wrap;
-  gap: 79px;
+  gap: 0 79px;
   justify-content: center;
   align-items: center;
 `;
 
 export const CardWhoWheAre = styled.div`
-  padding: 0;
+  padding: clamp(57px, 76vw, 76px) clamp(67.5px, 9vw, 90px);
   margin: 0;
   background-color: #a38334;
   max-width: 587px;
-  max-height: 333px;
+  min-height: 333px;
   top: 864px;
   font-size: 18px;
+  color: white;
   transform: translateY(-74px);
+  box-sizing: border-box;
 
-  p {
-    padding: 0;
+  h2 {
+    font-weight: 100;
+    font-size: 19px;
+    text-transform: uppercase;
+    margin-top: 0;
+  }
+  hr {
+    width: 34px;
+    height: 3px;
+    background: white;
+    border: none;
     margin: 0;
-    color: white;
+  }
+  p {
     font-size: 18px;
-    padding: 80px;
+    line-height: 30px;
+    font-weight: 300;
   }
 `;
 
