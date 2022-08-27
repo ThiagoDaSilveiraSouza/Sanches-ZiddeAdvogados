0cd1a16a1b0487e575cd7118bbfa4ad9b2b676a8
Paula <plnhschltz@gmail.com>
2022-08-27 02:25:14 -0300

    refactory with Thiago

--- src/pages/Home/components/Credentials/style.ts
+++ src/pages/Home/components/Credentials/style.ts
@@ -1,42 +1,49 @@
 import styled from "styled-components";
 
 export const CredentialsContent = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 43px;
-  justify-content: center;
-  align-items: center;
-  padding-top: 120px;
-  padding-bottom: 120px;
+  padding: 120px 0;
+
+  > .centralizer {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 43px;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export const ImgContent = styled.section``;
 export const TextCredentialsContent = styled.section`
-h2{
+  h2 {
     margin: 0;
     padding: 0;
     font-size: 28px;
     font-family: var(--secondary-font);
     color: #191919;
-}
-h5{
+  }
+  h5 {
     padding: 0;
     margin: 0;
     margin-top: 5px;
     font-size: 15px;
-}
-hr{
+  }
+  hr {
     padding: 0;
     margin: 0;
     margin-inline-start: -78px;
     height: 1px;
-    width: 539px;
-    background: #A38334;
+    width: 100%;
+    max-width: 539px;
+    background: #a38334;
     opacity: 1;
-}
-p{
+
+    @media (max-width: 1069px) {
+      margin-inline-start: 0;
+    }
+  }
+  p {
     padding: 0;
     max-width: 622px;
     font-size: 15px;
-}
+  }
 `;
