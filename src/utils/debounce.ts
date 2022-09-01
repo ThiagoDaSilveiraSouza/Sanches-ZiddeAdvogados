592b94a81ac181606dc49fa93fa27d52ef17207e
ThiagoSouza <thiagodasilveirasouza@gmail.com>
2022-08-31 22:37:43 -0300

    remove debounce

--- src/utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-export function debounce(func: Function, timeout = 200) {
-  let timer: number;
-  return (...args: any[]) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, args);
-    }, timeout);
-  };
-}
