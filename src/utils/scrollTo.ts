a2c605ee9892bcd80353567215efbb365b577529
ThiagoSouza <thiagodasilveirasouza@gmail.com>
2022-11-02 22:58:22 -0300

    update scrollTo util

--- src/utils/scrollTo.ts
+++ src/utils/scrollTo.ts
@@ -1,5 +1,9 @@
 export const scrollTo = (targetId: string) => {
   const targetElement = document.querySelector(targetId) as HTMLDivElement;
   const targetElementDistance = targetElement?.offsetTop;
-  window.scroll(0, targetElementDistance);
+  const targetScrollY =
+    targetElementDistance >= 80
+      ? targetElementDistance - 80
+      : targetElementDistance;
+  window.scroll(0, targetScrollY);
 };
